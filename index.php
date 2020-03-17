c1f4792c29ffbf1ebaff2cbd28919232c2f5c33d
robings <r_gordon_stables@hotmail.com>
2020-03-17 14:10:40 +0000

    added main divs and text and styled

--- index.php
+++ index.php
@@ -16,6 +16,28 @@ require_once __DIR__ . '/vendor/autoload.php';
     <link rel='stylesheet' type='text/css' href='styles.css' />
 </head>
 <body>
+<div class='container'>
+<header>
+<h1>Make a Meal of It</h1>
+</header>
 
+<article>
+    Wondering what to have for dinner? Make a Meal of It will provide recipes based on the ingredients you have in your store cupboard.
+    Tick the ingredients you want to use in your recipe, when done, click <span>Search</span> to find matching recipes.
+    Recipes provided by <a href='http://recipepuppy.com' target='_blank'>recipepuppy.com</a>.
+</article>
+
+<main>
+    <h3>Select your ingredients:</h3>
+    <form>
+    <div class='ingredients'>
+
+    </div>
+        <button>Search</button>
+    </form>
+
+</main>
+
+</div>
 </body>
 </html>
