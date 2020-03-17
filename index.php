00957e20f2442448e25abe9142af7f0d60fb98c1
ajhswan <andrewswan.dev@gmail.com>
2020-03-17 14:56:03 +0000

    small changes after code reivew and removed var_dump($connection) post testing

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-require_once __DIR__. '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use \Mamoi\Resources\Db;
 
 $db = new Db();
 $connection = $db->getConnection();
-
-var_dump($connection); // for testing purposes only delete once done
