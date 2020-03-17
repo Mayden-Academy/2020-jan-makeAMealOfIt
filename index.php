614644c5743f0e623c1dfd38634686a5bce26947
markandmarkup <markeyles88@gmail.com>
2020-03-17 15:43:17 +0000

    Hotfix made to adapt IngredientEntityHydrator to use PDO type hinting and added procedural lines to index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use \Mamoi\Resources\Db;
+use \Mamoi\Hydrators\IngredientEntityHydrator;
 
 $db = new Db();
 $connection = $db->getConnection();
+$ingredientHydrator = new IngredientEntityHydrator($connection);
+$allIngredients = $ingredientHydrator->getAllIngredientEntities();
+
+var_dump($allIngredients);
