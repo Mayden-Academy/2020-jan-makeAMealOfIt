1a85e5504acba90285be4145081ad659d337319a
robings <r_gordon_stables@hotmail.com>
2020-03-17 13:28:18 +0000

    added namespace, autoload.php require and basic html, added normalize.css

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mamoi;
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Make a Meal of It</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' type='text/css' href='normalize.css' />
+    <link rel='stylesheet' type='text/css' href='styles.css' />
+</head>
+<body>
+
+</body>
+</html>
