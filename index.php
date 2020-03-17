5d7300a99b14dee0ef9567e6864b2e50826b7956
robings <r_gordon_stables@hotmail.com>
2020-03-17 15:09:29 +0000

    tested and fixed styling and code spacing and indents

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 namespace Mamoi;
 require_once __DIR__ . '/vendor/autoload.php';
 
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Make a Meal of It</title>
@@ -17,30 +16,32 @@ require_once __DIR__ . '/vendor/autoload.php';
     <link rel='stylesheet' type='text/css' href='normalize.css' />
     <link rel='stylesheet' type='text/css' href='styles.css' />
 </head>
+
 <body>
-<div class='container'>
-<header>
-<h1>Make a Meal of It</h1>
-</header>
+    <div class='container'>
 
-<article>
-    <p>Wondering what to have for dinner?</p>
-    <p>Make a Meal of It will provide recipes based on the ingredients you have in your store cupboard.
-        Tick the ingredients you want to use in your recipe, when done, click <span>SEARCH</span> to find matching recipes.</p>
-    <p>Recipes provided by <a href='http://recipepuppy.com' target='_blank'>recipepuppy.com</a>.</p>
-</article>
+        <header>
+            <h1>Make a Meal of It</h1>
+        </header>
 
-<main>
-    <h3>Select your ingredients:</h3>
-    <form method='get' action='recipe.php'>
-    <div class='ingredients'>
+        <article>
+            <p>Wondering what to have for dinner?</p>
+            <p>Make a Meal of It will provide recipes based on the ingredients you have in your store cupboard.
+                Tick the ingredients you want to use in your recipe, when done, click <span>SEARCH</span> to find matching recipes.</p>
+            <p>Recipes provided by <a href='http://recipepuppy.com' target='_blank'>recipepuppy.com</a>.</p>
+        </article>
+
+        <main>
+            <h3>Select your ingredients:</h3>
+            <form method='get' action='recipe.php'>
+                <div class='ingredients'>
+
+                </div>
+                <button>Search</button>
+            </form>
+        </main>
 
     </div>
-        <button>Search</button>
-    </form>
-
-</main>
-
-</div>
 </body>
+
 </html>
