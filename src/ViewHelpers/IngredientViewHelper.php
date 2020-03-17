692a3c52e68b71830c71b0ac6530b4794bf08032
astonelunde <alexlunde@gmail.com>
2020-03-17 13:45:06 +0000

    initial build of IngredientViewHelper class

---
+++ src/ViewHelpers/IngredientViewHelper.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mamoi\Entities;
+
+class IngredientViewHelper
+{
+    static function displayAllIngredients(array $allTheIngredients) {
+        $result = '';
+
+        foreach ($allTheIngredients as $ingredient) {
+            $result .= self::generateIngredientHtml($ingredient);
+        }
+
+        return $result;
+    }
+
+    static function generateIngredientHtml (array $ingredient) {
+
+        $id = $ingredient->getId();
+        $name = $ingredient->getName();
+
+        return
+        '<div class=\"ingredient\">
+            <label>
+            <input type=\"checkbox\" name=\"' . $id . '\" id=\"' . $name . '\">'
+                . $name . '
+            </label>
+        </div>';
+
+    }
+}
